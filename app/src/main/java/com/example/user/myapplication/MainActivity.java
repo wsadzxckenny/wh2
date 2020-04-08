1331fb1a0eab8b0cf16ca2ba30ae532ed832e186
wsadzxckenny <s5309pp@gmail.com>
2020-04-08 22:26:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.user.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    String ans ="1234";
+    public void myfun(View btnView){
+
+        EditText et=(EditText) findViewById(R.id.editText);
+        TextView tv=(TextView) findViewById(R.id.textView);
+        char[] guess=et.getText().toString().toCharArray();
+        char[] ansArray= ans.toCharArray();
+        int nA =0;
+        int nB =0;
+        for(int i=0; i < guess.length; i++) {
+            for (int j = 0; j < ansArray.length; j++) {
+                if ( guess[i]==ansArray[j])
+                {
+                    if ( i==j)
+                        nA++;
+                    else
+                        nB++;
+                }
+            }
+        }
+        String ret =nA +"A"+nB+"B";
+        tv.setText(ret);
+    }
+
+}
